4a27d7580c4af78c2a5cbe207b773daa51d053cf
thegamer256 <lanhtuan65@gmail,com>
2023-03-23 16:36:56 +0700

    update donate

--- src/java/Donate/DonateDAO.java
+++ src/java/Donate/DonateDAO.java
@@ -5,10 +5,46 @@
  */
 package Donate;
 
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author LE ANH TUAN
  */
 public class DonateDAO {
-    
+    public int getWalletAmount(int accountId) {
+        try {
+            Connection conn;
+            PreparedStatement ps;
+            ResultSet rs;
+
+            String query = "select bank_monney_amount from donor where account_id=?";
+
+            conn = new DBContext().getConnection();
+
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, accountId);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                return rs.getInt("bank_monney_amount");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DonateDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DonateDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
 }
--- src/java/User/UserController.java
+++ src/java/User/UserController.java
@@ -32,7 +32,7 @@ public class UserController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
