278ca1d65a40425064088f00cc0dd348abbad34e
huynhduc <huynhduc.penta@gmail.com>
2023-03-23 08:52:25 +0700

    update Model news

--- src/java/News/News.java
+++ src/java/News/News.java
@@ -5,11 +5,20 @@
 package News;
 
 import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  *
  * @author huynh
  */
+
+@Builder
+@Getter
+@Setter
+@ToString
 public class News {
 
     private int newsId;
@@ -20,79 +29,5 @@ public class News {
     private int userId;
     private List<String> imgsPath;
 
-    public News() {
-    }
 
-    public News(int newsId, String newsTitle, String newsDes, String shortDes, String postedDate, int userId, List<String> imgsPath) {
-        this.newsId = newsId;
-        this.newsTitle = newsTitle;
-        this.newsDes = newsDes;
-        this.shortDes = shortDes;
-        this.postedDate = postedDate;
-        this.userId = userId;
-        this.imgsPath = imgsPath;
-    }
-
-    public int getNewsId() {
-        return newsId;
-    }
-
-    public void setNewsId(int newsId) {
-        this.newsId = newsId;
-    }
-
-    public String getNewsTitle() {
-        return newsTitle;
-    }
-
-    public void setNewsTitle(String newsTitle) {
-        this.newsTitle = newsTitle;
-    }
-
-    public String getNewsDes() {
-        return newsDes;
-    }
-
-    public void setNewsDes(String newsDes) {
-        this.newsDes = newsDes;
-    }
-
-    public String getShortDes() {
-        return shortDes;
-    }
-
-    public void setShortDes(String shortDes) {
-        this.shortDes = shortDes;
-    }
-
-    public String getPostedDate() {
-        return postedDate;
-    }
-
-    public void setPostedDate(String postedDate) {
-        this.postedDate = postedDate;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public List<String> getImgsPath() {
-        return imgsPath;
-    }
-
-    public void setImgsPath(List<String> imgsPath) {
-        this.imgsPath = imgsPath;
-    }
-
-    @Override
-    public String toString() {
-        return "News{" + "newsId=" + newsId + ", newsTitle=" + newsTitle + ", newsDes=" + newsDes + ", shortDes=" + shortDes + ", postedDate=" + postedDate + ", userId=" + userId + ", imgsPath=" + imgsPath + '}';
-    }
-    
-    
 }
