1904ff140849bba05ed8d76d236579a5050bd47a
tmanhtu <128576763+tmanhtu@users.noreply.github.com>
2023-03-27 16:03:48 +0700

    Update Operator.java

--- src/java/Operator/Operator.java
+++ src/java/Operator/Operator.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Operator;
 
 import java.util.List;
@@ -12,10 +8,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 
-@Builder
-@Getter
-@Setter
-@ToString
+
 public class Operator {
     private int operatorId;
     private int programId;
